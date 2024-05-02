13497d0d439fd7ba2cb257a34f71ea8c2483cdf3
rcdhtx <rcdhtx@gmail.com>
2024-05-02 01:42:40 -0400

    updates

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import os
 
+from nextcord import Interaction, SlashOption
+from nextcord.ext import commands
 import google.generativeai as genai
+from google.generativeai.types import HarmBlockThreshold, HarmCategory
 
-import discord
-from discord.ext import commands
 
 discord_bot_key = os.environ["DISCORD_BOT_KEY"]
 
-intents = discord.Intents.default()
-intents.message_content = True
+bot = commands.Bot()
 
-bot = commands.Bot(command_prefix=">", intents=intents)
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
-
 model = genai.GenerativeModel("gemini-pro")
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+@bot.slash_command(description="Ping command")
+async def ping(interaction: Interaction):
+    await interaction.response.send_message("Pong!")
 
-    if message.content.startswith("!gemini"):
-        response = model.generate_content(message.content[8:])
-        try:
-            await message.channel.send(response.text)
-        except discord.errors.HTTPException:
-            await message.channel.send(response.text)
+
+@bot.slash_command(description="prompts google gemini ai")
+async def gemini(
+    interaction: Interaction, arg: str = SlashOption(description="Prompt")
+):
+    response = model.generate_content(
+        arg,
+        safety_settings={
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+        },
+    )
+
+    await interaction.response.send_message(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
-    try:
-        bot.run(discord_bot_key)
-    except:
-        os.system("kill 1")
+    bot.run(discord_bot_key)
